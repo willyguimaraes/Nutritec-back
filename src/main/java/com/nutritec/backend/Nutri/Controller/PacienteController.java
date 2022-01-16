a6bfeb6cdadb2d572bfbdceab5b8882ce9edb817
willyguimaraes <willy.barros@ccc.ufcg.edu.br>
2022-01-15 21:59:05 -0300

    Update iniciado

---
+++ src/main/java/com/nutritec/backend/Nutri/Controller/PacienteController.java
@@ -0,0 +1,52 @@
+package com.nutritec.backend.Nutri.Controller;
+
+import java.util.List;
+import com.nutritec.backend.Nutri.Model.Paciente;
+import com.nutritec.backend.Nutri.Repository.PacienteRepository;
+import com.nutritec.backend.Nutri.Service.PacienteService;
+import com.nutritec.backend.Util.CustomError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/paciente")
+@AllArgsConstructor
+public class PacienteController {
+
+    PacienteService service;
+    PacienteRepository repository;
+
+
+    @PostMapping
+    public ResponseEntity<?> cadastrarPaciente(@RequestBody Paciente paciente, UriComponentsBuilder ucBuilder) {
+
+       List<Paciente> pessoas = repository.findByCpf(paciente.getCpf());
+
+        if (!pessoas.isEmpty()) {
+            return new ResponseEntity<CustomError>(new CustomError("ERRO! Paciente já cadastrado"), HttpStatus.CONFLICT);
+        }
+
+        repository.save(paciente);
+        return new ResponseEntity<Paciente>(paciente, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{id}")
+	public ResponseEntity<?> AtualizarNutri(@RequestBody Paciente Paciente) {
+
+        List<Paciente> pessoas = repository.findByMatricula(Paciente.);
+
+        if (pessoas.isEmpty()) {
+            return new ResponseEntity<CustomError>(new CustomError("ERRO! Paciente não cadastrado"), HttpStatus.CONFLICT);
+        }
+
+    } 
+    
+}
